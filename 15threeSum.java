ecf9d951539bb33277bf9229a351e452951c3255
Hongsen <hhsen8816@gmail.com>
2015-03-07 13:35:11 -0800

    0307

--- 15threeSum.java
+++ 15threeSum.java
@@ -58,3 +58,67 @@ public class Solution {
 		return result;
 	}
 }
+
+// another solution
+/*
+这道题是Two Sum的扩展，brute force时间复杂度为O(n^3), 对每三个数进行比较。这道题
+和Two Sum有所不同，使用哈希表的解法并不是很方便，因为结果数组中元素可能重复，
+如果不排序对于重复的处理将会比较麻烦，因此这道题一般使用排序之后夹逼的方法，
+总的时间复杂度为O(n^2+nlogn)=(n^2),空间复杂度是O(n),代码如下：
+*/
+
+public class Solution {
+	public ArrayList<ArrayList<Integer>> threeSum(int[] num)  
+	{  
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();  
+		if(num==null || num.length<=2)  
+			return res;  
+		Arrays.sort(num);  
+		for(int i=num.length-1;i>=2;i--)  
+		{  
+			if(i<num.length-1 && num[i]==num[i+1])  
+				continue;  
+			ArrayList<ArrayList<Integer>> curRes = twoSum(num,i-1,-num[i]);  
+			for(int j=0;j<curRes.size();j++)  
+			{  
+				curRes.get(j).add(num[i]);  
+			}  
+			res.addAll(curRes);  
+		}  
+		return res;  
+	}  
+	private ArrayList<ArrayList<Integer>> twoSum(int[] num, int end,int target)  
+	{  
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();  
+		if(num==null || num.length<=1)  
+			return res;  
+		int l = 0;  
+		int r = end;  
+		while(l<r)  
+		{  
+			if(num[l]+num[r]==target)  
+			{  
+				ArrayList<Integer> item = new ArrayList<Integer>();  
+				item.add(num[l]);  
+				item.add(num[r]);  
+				res.add(item);  
+				l++;  
+				r--;  
+				while(l<r&&num[l]==num[l-1])  
+					l++;  
+				while(l<r&&num[r]==num[r+1])  
+					r--;  
+			}  
+			else if(num[l]+num[r]>target)  
+			{  
+				r--;  
+			}  
+			else  
+			{  
+				l++;  
+			}  
+		}  
+		return res;  
+	}  
+
+}
