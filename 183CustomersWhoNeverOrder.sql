e2a0b7e8b4dc8a72e3ea4ad05735b699dc4e890a
Hongsen <hhsen8816@gmail.com>
2015-03-26 00:33:56 -0700

    0326

---
+++ 183CustomersWhoNeverOrder.sql
@@ -0,0 +1,45 @@
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+# Write your MySQL query statement below
+# LEFT JOIN
+SELECT Name
+FROM Customers c 
+LEFT JOIN Orders o
+ON c.Id = o.CustomerId
+WHERE CustomerID IS NULL;
+/*
+使用NOT IN（Accepted 777ms）：
+# Write your MySQL query statement below
+SELECT Name FROM Customers c WHERE c.Id NOT IN (SELECT CustomerId FROM Orders o);
+
+使用NOT EXISTS（Accepted 745ms）：
+# Write your MySQL query statement below
+SELECT Name FROM Customers c WHERE NOT EXISTS (SELECT CustomerId FROM Orders o WHERE o.CustomerId = c.id);
+*/
